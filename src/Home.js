423b1a05a0448dda9bbdec60fad5cb8018a135f0
olbesp <olbesp365@gmail.com>
2018-01-25 23:12:28 +0200

    Create random message displaying

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import './Home.css';
 
 export class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: [
+        'A simple guy who wants to create really cool things',
+        'Elegant solutions in the least amount of time',
+        '#1 goal - to meet your needs and deadline'
+      ]
+    }
+    this.selectRandomMessage = this.selectRandomMessage.bind(this);
+  }
+
+  selectRandomMessage() {
+    return Math.floor(Math.random() * this.state.message.length);
+  }
+
   render() {
     return (
       <div className="Home">
@@ -10,7 +26,7 @@ export class Home extends Component {
           <h1 className="heading-primary">
             <span className="heading-primary--sub-1">Frontend Developer</span>
             <span className="heading-primary--main">Oleksii Bespalko</span>
-            <span className="heading-primary--sub">A simple guy who wants to create really cool things</span>
+            <span className="heading-primary--sub">{this.state.message[this.selectRandomMessage()]}</span>
           </h1>
           <a data-scroll href="#section-projects" id="Home-btn" className="btn btn--blue btn--animated">Projects <span className="btn__arrow">&darr;</span></a>
         </div>
