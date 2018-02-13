43cbc3ef9471c5238b4454f10cf7d99ed4b8cfde
olbesp <olbesp365@gmail.com>
2018-02-13 19:09:10 +0200

    Create main heading style

--- src/components/MainHeading/MainHeading.js
+++ src/components/MainHeading/MainHeading.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import styles from './MainHeading.css';
+
 const mainHeading = (props) => (
-  <div>
-    <h3>{props.role}</h3>
-    <h1>{props.name}</h1>
-    <h5>{props.message}</h5>
+  <div className={styles.MainHeading}>
+    <h3 className={styles.Role}>{props.role}</h3>
+    <h1 className={styles.Name}>{props.name}</h1>
+    <h5 className={styles.Message}>{props.message}</h5>
   </div>
 );
 
