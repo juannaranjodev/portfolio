f3d0336dea2562f57614a3bd0fa5196bac892c1f
olbesp <olbesp365@gmail.com>
2018-02-13 15:25:05 +0200

    Reset project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import fontawesome from '@fortawesome/fontawesome';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import brands from '@fortawesome/fontawesome-free-brands';
-import { faArrowRight } from '@fortawesome/fontawesome-free-solid';
-
-
-import { Nav } from './Nav/Nav';
-import { Home } from './Home/Home';
-import { Footer } from './Footer/Footer';
-
-
-fontawesome.library.add(brands, faArrowRight);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Nav />
-        <Home />
-        <Footer />
+        
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import WebFont from 'webfontloader';
-
-WebFont.load({
-  google: {
-    families: ['Montserrat:100,300,400,700', 'sans-serif']
-  }
-});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
