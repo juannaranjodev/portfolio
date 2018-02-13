d08a0840062c76ec1f3a06b08cb3bd4b9960763f
olbesp <olbesp365@gmail.com>
2018-02-14 00:39:34 +0200

    Switch to video background

--- src/components/UI/Background/Background.js
+++ src/components/UI/Background/Background.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
-import BackgroundImage from '../../../assets/background.jpeg';
+import BackgroundVideo from '../../../assets/videos/background.mp4';
 import styles from './Background.css';
 
 const background = () => {
-  const componentStyle = {
-    backgroundImage: `url(${BackgroundImage})`
-  };
+  // const componentStyle = {
+  //   backgroundImage: `url(${BackgroundImage})`
+  // };
 
-  return <div className={styles.Background} style={componentStyle}></div>;
+  return (
+    <div className={styles.Background}>
+      <video autoPlay muted loop>
+        <source src={BackgroundVideo} type="video/mp4" />
+          Your browser is not supported!
+      </video>
+    </div>
+  );
 }
 
 export default background;
