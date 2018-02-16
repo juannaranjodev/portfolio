42d9419d6a860c360e4939aaf88e625996acbc3c
olbesp <olbesp365@gmail.com>
2018-02-16 11:05:34 +0200

    Change the background of home page to the image

--- src/components/UI/Background/Background.js
+++ src/components/UI/Background/Background.js
@@ -1,19 +1,16 @@
 import React from 'react';
 
-import BackgroundVideo from '../../../assets/videos/background.mp4';
+import BackgroundImage from '../../../assets/images/background.jpg';
 import styles from './Background.css';
 
 const background = () => {
-  // const componentStyle = {
-  //   backgroundImage: `url(${BackgroundImage})`
-  // };
+  const componentStyle = {
+    backgroundImage: `url(${BackgroundImage})`
+  };
 
   return (
-    <div className={styles.Background}>
-      <video autoPlay muted loop>
-        <source src={BackgroundVideo} type="video/mp4" />
-          Your browser is not supported!
-      </video>
+    <div className={styles.Background}
+      style={componentStyle}>
     </div>
   );
 }
