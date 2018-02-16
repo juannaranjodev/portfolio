f6ada8da74da9bb2ff12e8b1f23024a8bd4d7b60
olbesp <olbesp365@gmail.com>
2018-02-16 11:28:23 +0200

    Create the toolbar component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux';
+import Toolbar from '../Toolbar/Toolbar';
 
 const layout = (props) => (
   <Aux>
-    <header>Nav, logo</header>
+    <Toolbar />
     <main>{props.children}</main>
     <footer>Social media links, copyrights</footer>
   </Aux>
---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import styles from './Toolbar.css';
+
+const toolbar = (props) => (
+  <header className={styles.Toolbar}>
+    <div>
+      LOGO
+    </div>
+    <nav>
+      ...
+    </nav>
+  </header>
+);
+
+export default toolbar;
