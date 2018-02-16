b7e554a366a19af7eadcd9ef0840548a0d52e971
olbesp <olbesp365@gmail.com>
2018-02-16 13:55:33 +0200

    Restructure navigation directory

---
+++ src/components/Navigation/NavigationMenu/NavigationMenu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './NavigationMenu.css';
+
+const navigationMenu = () => (
+  <nav>
+    
+  </nav>
+);
+
+export default navigationMenu;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import styles from './Toolbar.css';
-import Logo from '../Logo/Logo';
+import Logo from '../../Logo/Logo';
 
 const toolbar = (props) => (
   <header className={styles.Toolbar}>
