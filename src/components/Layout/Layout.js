6548ccc8e6f18c27564453b782de1721528d562c
olbesp <olbesp365@gmail.com>
2018-02-16 23:05:00 +0200

    Create a social media component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SocialMedia from '../SocialMedia/SocialMedia';
 
 const layout = (props) => (
   <Aux>
     <Toolbar />
     <main>{props.children}</main>
-    <footer>Social media links, copyrights</footer>
+    <SocialMedia />
   </Aux>
 );
 
---
+++ src/components/SocialMedia/SocialMedia.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './SocialMedia.css';
+
+const socialMedia = () => (
+  <footer className={styles.SocialMedia}>OLBESp</footer>
+);
+
+export default socialMedia;
