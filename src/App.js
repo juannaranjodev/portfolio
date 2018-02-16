116a630567dad45a0e55f5cfcb81f253b2f64173
olbesp <olbesp365@gmail.com>
2018-02-17 00:04:14 +0200

    Add social media icons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 import Layout from './components/Layout/Layout';
 import Home from './containers/Home/Home';
 
+import fontawesome from '@fortawesome/fontawesome';
+import brands from '@fortawesome/fontawesome-free-brands';
+
+fontawesome.library.add(brands);
+
 class App extends Component {
   render() {
     return (
--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
 import styles from './SocialMedia.css';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 
-const socialMedia = () => (
-  <footer className={styles.SocialMedia}>OLBESp</footer>
-);
+const socialMedia = () => {
+  const socialProfiles = [
+    { link: "https://github.com/olbesp", icon: ["fab", "github"] },
+    { link: "https://www.linkedin.com/in/olbesp/", icon: ["fab", "linkedin"] },
+    { link: "https://www.facebook.com/olbesp", icon: ["fab", "facebook"] }
+  ];
+
+  const icons = socialProfiles.map((profile, index) => {
+    return (
+      <li key={index}>
+        <a href={profile.link}
+          target="_blank"
+          rel="noopener noreferrer">
+          <FontAwesomeIcon icon={profile.icon} />
+        </a>
+      </li>    
+    );
+  });
+
+  return (
+    <footer className={styles.SocialMedia}>
+      <div>Made by me © 2018</div>
+      <ul>
+        {icons}
+      </ul>
+    </footer>
+  );
+};
+
 
 export default socialMedia;
