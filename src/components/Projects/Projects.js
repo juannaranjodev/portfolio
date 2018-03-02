3e11a1a82e46c7f19970c0a12281e496e4b9914c
olbesp <olbesp365@gmail.com>
2018-03-02 15:55:26 +0200

    Get data from Firebase

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,9 +5,38 @@ import axios from 'axios';
 // import Project from './Project/Project';
 
 class Projects extends Component {
+  state = {
+    projects: null
+  }
+
+  getProjects = () => {
+    axios.get('https://olbesp-portfolio.firebaseio.com/projects.json')
+      .then(response => {
+        const projects = response.data;
+        this.setState({ projects });
+        console.log(this.state.projects);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  componentDidMount() {
+    this.getProjects();
+  }
+
   render() {
+
+    let projectsUI = <div>Loading...</div>
+    if (this.state.projects) {
+      projectsUI = this.state.projects
+        .filter(project => project.image)
+        .map(project => <div key={project.id}>{project.title}</div>);
+    }
+
     return (
       <div className={styles.Projects}>
+        {projectsUI}
       </div>
     );
   }
