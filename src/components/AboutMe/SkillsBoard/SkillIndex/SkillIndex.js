55db50ec6478e9ff3bd4b78beb0cefe4cd180c55
olbesp <olbesp365@gmail.com>
2018-03-07 16:48:25 +0200

    Recreate skill indicators

--- src/components/AboutMe/SkillsBoard/SkillIndex/SkillIndex.js
+++ src/components/AboutMe/SkillsBoard/SkillIndex/SkillIndex.js
@@ -4,12 +4,18 @@ import styles from './SkillIndex.css';
 
 const skillIndex = (props) => (
   <div className={styles.SkillIndex}>
-    <div className={styles.Indicator}></div>
+    <div className={styles.Indicator}>
+      <div className={styles.Progress}>
+        <div className={styles.ProgressBar} style={{
+          bottom: `calc(${props.value}% - 2rem)`,
+          transform: 'translateY(-50%)'
+        }}></div>
+      </div>
+    </div>
     <div className={styles.SkillTitle}>{props.title}</div>
   </div>
 );
 
 export default skillIndex;
 
-// 
-// { props.value }
+// <div className={styles.ProgressValue}>{`${props.value}%`}</div>
