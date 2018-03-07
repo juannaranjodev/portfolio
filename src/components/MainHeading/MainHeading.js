e3e6d85e2dc76367e26aeac6643f29e99b81097e
olbesp <olbesp365@gmail.com>
2018-03-07 11:44:58 +0200

    Fix reference in MainHeading Button

--- src/components/MainHeading/MainHeading.js
+++ src/components/MainHeading/MainHeading.js
@@ -9,8 +9,7 @@ const mainHeading = (props) => (
     <h1 className={styles.Name}>{props.name}</h1>
     <h5 className={styles.Message}>{props.message}</h5>
     <Button
-      type="button"
-      clicked={() => alert('Hi')}
+      href="/portfolio"
       color="Info">
       Projects
     </Button>
