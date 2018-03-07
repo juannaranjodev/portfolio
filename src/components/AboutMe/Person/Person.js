d545e703aa23cc3c1d614f9871f35e6cfff43ffb
olbesp <olbesp365@gmail.com>
2018-03-08 00:39:01 +0200

    Create eyes for Person image

--- src/components/AboutMe/Person/Person.js
+++ src/components/AboutMe/Person/Person.js
@@ -4,6 +4,28 @@ import styles from './Person.css';
 
 const person = () => (
   <div className={styles.Container}>
+    <div className={styles.Person}>
+      <div className={styles.Person__head}>
+        <div className={styles.Person__hair}></div>
+        <div className={styles.Person__eyes}>
+          <div className={styles.Eye}>
+            <div className={styles.Eye__apple}></div>
+          </div>
+          <div className={styles.Eye}>
+            <div className={styles.Eye__apple}></div>
+          </div>
+        </div>
+        <div className={styles.Person__head__ear_left}></div>
+        <div className={styles.Person__head__ear_right}></div>
+      </div>
+      <div className={styles.Person__body}></div>
+      <div className={styles.Person__shoulder_left}></div>
+      <div className={styles.Person__shoulder_right}></div>
+      <div className={styles.Person__arm_left}></div>
+      <div className={styles.Person__arm_right}></div>
+      <div className={styles.Person__leg_left}></div>
+      <div className={styles.Person__leg_right}></div>
+    </div>
     <div className={styles.Table}>
       <div className={styles.Laptop}>
         <div className={styles.Laptop__sticker}>JS</div>
