dad96b0880ebd3d497206babb4236731978af048
olbesp <olbesp365@gmail.com>
2018-03-07 19:35:11 +0200

    Add hover effect into Skill Indicator

--- src/components/AboutMe/SkillsBoard/SkillIndex/SkillIndex.js
+++ src/components/AboutMe/SkillsBoard/SkillIndex/SkillIndex.js
@@ -3,15 +3,16 @@ import React from 'react';
 import styles from './SkillIndex.css';
 
 const skillIndex = (props) => {
-  let progressBar = {
-    bottom: `calc(${props.value}% - 2rem)`,
-    transform: 'translateY(-50%)'
-  };
+  // let progressBar = {
+  //   bottom: `calc(${props.value}% - 2rem)`,
+  //   transform: 'translateY(-50%)'
+  // };
   return (
     <div className={styles.SkillIndex}>
       <div className={styles.Indicator}>
         <div className={styles.Progress}>
-          <div className={styles.ProgressBar} style={progressBar}></div>
+          <div className={styles.ProgressValue} style={{height: `calc(${props.value}% - 2rem)`}}></div>
+          <div className={styles.ProgressBar}></div>
         </div>
       </div>
       <div className={styles.SkillTitle}>{props.title}</div>
@@ -20,5 +21,3 @@ const skillIndex = (props) => {
 }
 
 export default skillIndex;
-
-// <div className={styles.ProgressValue}>{`${props.value}%`}</div>
