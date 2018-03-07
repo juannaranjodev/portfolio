6e4fff91dc81e87ba5599ed9e1b8f29a82efdefa
olbesp <olbesp365@gmail.com>
2018-03-07 17:07:22 +0200

    Fix skill progress position

--- src/components/AboutMe/SkillsBoard/SkillIndex/SkillIndex.js
+++ src/components/AboutMe/SkillsBoard/SkillIndex/SkillIndex.js
@@ -2,19 +2,22 @@ import React from 'react';
 
 import styles from './SkillIndex.css';
 
-const skillIndex = (props) => (
-  <div className={styles.SkillIndex}>
-    <div className={styles.Indicator}>
-      <div className={styles.Progress}>
-        <div className={styles.ProgressBar} style={{
-          bottom: `calc(${props.value}% - 2rem)`,
-          transform: 'translateY(-50%)'
-        }}></div>
+const skillIndex = (props) => {
+  let progressBar = {
+    bottom: `calc(${props.value}% - 2rem)`,
+    transform: 'translateY(-50%)'
+  };
+  return (
+    <div className={styles.SkillIndex}>
+      <div className={styles.Indicator}>
+        <div className={styles.Progress}>
+          <div className={styles.ProgressBar} style={progressBar}></div>
+        </div>
       </div>
+      <div className={styles.SkillTitle}>{props.title}</div>
     </div>
-    <div className={styles.SkillTitle}>{props.title}</div>
-  </div>
-);
+  );
+}
 
 export default skillIndex;
 
