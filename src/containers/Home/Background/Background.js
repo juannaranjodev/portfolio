5e6f8d69e2ae21d0f21cab9ce80556646cb0ddfc
olbesp <olbesp365@gmail.com>
2018-03-13 22:23:38 +0200

    Replace city image into function

--- src/containers/Home/Background/Background.js
+++ src/containers/Home/Background/Background.js
@@ -6,31 +6,34 @@ import Character from '../../../components/UI/Character/Character';
 const random = limit => Math.floor(Math.random() * limit);
 
 const background = (props) => {
-  let houses = [];
-  let widthReminder = window.innerWidth;
-  let key = 0;
-  while (widthReminder > 0) {
-    let houseWidth = random(10) + 30;
-    let houseHeight = random(150) + random(50);
-    let aerialHeight = random(houseHeight);
-    let aerialTop = -random(aerialHeight / 3);
-    houses.push(
-      <div key={key} className={styles.house} style={{ width: houseWidth, height: houseHeight }}>
-        <div className={styles.aerial} style={{
-          width: `${random(4)}px`,
-          height: aerialHeight,
-          top: aerialTop,
-          left: `${random(100)}%`
-        }}></div>
-      </div>);
-    widthReminder -= houseWidth;
-    key++;
+
+  const createCity = () => {
+    let houses = [];
+    let widthReminder = window.innerWidth;
+    while (widthReminder > 0) {
+      let houseWidth = random(10) + 30;
+      let houseHeight = random(150) + random(50);
+      let aerialHeight = random(houseHeight);
+      let aerialTop = -random(aerialHeight / 3);
+      houses.push(
+        <div key={houses.length} className={styles.house} style={{ width: houseWidth, height: houseHeight }}>
+          <div className={styles.aerial} style={{
+            width: `${random(4)}px`,
+            height: aerialHeight,
+            top: aerialTop,
+            left: `${random(100)}%`
+          }}></div>
+        </div>);
+      widthReminder -= houseWidth;
+    }
+    return houses;
   }
+  
   return (
     <div className={styles.Background}>
       <div className={styles.sunlight}></div>
       <div className={styles.city}>
-        {houses}
+        {createCity()}
       </div>
       <Character />
     </div>
