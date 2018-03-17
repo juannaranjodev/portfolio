730a6820a677f549d0b375819dca1a01bd1b5e5d
olbesp <olbesp365@gmail.com>
2018-03-17 16:47:45 +0200

    Rebuild ProjectModal markup

--- src/components/ProjectModal/ProjectModal.js
+++ src/components/ProjectModal/ProjectModal.js
@@ -3,24 +3,27 @@ import React from 'react';
 import styles from './ProjectModal.css';
 import Button from '../UI/Button/Button';
 
+
+
 const projectModal = (props) => (
-  <div className={styles.ProjectModal} style={{
-    backgroundImage: `linear-gradient(rgba(0,0,0,.9), rgba(0,0,0,.9)), url(${props.image})`
-  }}>
-    <header className={styles.ProjectModal__header}>
-      <h3>{props.title}</h3>
-      <div className={styles.CloseBtn} onClick={props.closeModal}>
-        <Button type="button" color="Action">Close</Button>
-      </div>
-    </header>
-    <div className={styles.ProjectModal__description}>{props.description}</div>
-    <ul className={styles.ProjectModal__tools}>
-      {
-        props.tools.map((tool, index) => {
-          return <li key={index}>{tool}</li>
-        })
-      }
-    </ul>
+  <div className={styles.ProjectModal}>
+    <div 
+      className={styles.ProjectModal__image} 
+      style={{ backgroundImage: `url(${props.image})` }}
+    >
+    </div>
+    <div className={styles.ProjectModal__data}>
+      <header className={styles.data__header}>
+        <h3>{props.title}</h3>
+        <div className={styles.CloseBtn} onClick={props.closeModal}>
+          <Button type="button" color="Action">Close</Button>
+        </div>
+      </header>
+      <div className={styles.ProjectModal__description}>{props.description}</div>
+      <ul className={styles.ProjectModal__tools}>
+        { props.tools.map((tool, index) => <li key={index}>{tool}</li>) }
+      </ul>
+    </div>
     <footer className={styles.ProjectModal__footer}>
       <Button href={props.code} newtab color="Info">Code</Button>
       <Button href={props.app} newtab color="Info">Link</Button>
