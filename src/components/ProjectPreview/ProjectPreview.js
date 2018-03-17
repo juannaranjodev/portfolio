d612966032b8342de91e4573978057f41670755c
olbesp <olbesp365@gmail.com>
2018-03-17 16:02:34 +0200

    Create animation for ProjectPreview appearance

--- src/components/ProjectPreview/ProjectPreview.js
+++ src/components/ProjectPreview/ProjectPreview.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 import styles from './ProjectPreview.css';
+import { random } from '../../containers/App/App';
 
 const projectPreview = (props) => {
   let width = null;
   if (props.width) {
     width = props.width;
   }
+  const animatedClasses = ['fromTop', 'fromBottom', 'fromLeft', 'fromRight'];
   return (
-    <figure className={styles.Project} id={props.id} style={{ width }}>
+    <figure className={[styles.Project, styles[animatedClasses[random(animatedClasses.length)]]].join(' ')} id={props.id} style={{ width }}>
       <img className={styles.Project__img} src={props.img} alt={props.title} />
       <figcaption className={[styles.Project__title, 'textShadow'].join(' ')}>{props.title}</figcaption>
     </figure>
