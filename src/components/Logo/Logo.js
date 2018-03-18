c9f38c67cf370ebb7766aebfdcc5663c5ae6bcff
olbesp <olbesp365@gmail.com>
2018-03-18 13:19:37 +0200

    Add the animation to each letter of Logo firtname

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,11 +2,30 @@ import React from 'react';
 
 import styles from './Logo.css';
 
-const logo = () => (
-  <div className={[styles.Logo].join(' ')}>
-    <span className={styles.Logo__o}>Oleksii</span>
-    <span className={styles.Logo__b}>Bespalko</span>
-  </div>
-);
+const logo = () => {
+  const name = 'Oleksii Bespalko';
+  const splitWord = (word) => {
+    return word.split('').map((letter, index) => {
+      return (
+        <span key={index} 
+          className={styles.letter} 
+          style={{animationDelay: `${index / 5}s`}}
+        >
+          {letter}
+        </span>
+      );
+    });
+  }
+  return (
+    <div className={[styles.Logo].join(' ')}>
+      <div className={styles.Logo__firstname}>
+        {splitWord(name.split(' ')[0])}
+      </div>
+      <div className={styles.Logo__lastname}>
+        {name.split(' ')[1]}
+      </div>
+    </div>
+  );
+}
 
 export default logo;
