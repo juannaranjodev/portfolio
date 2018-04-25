46640d9e2c32512d9a493e4286f8f7b969e5ee54
Oleksii Bespalko <ol.bespalko@gmail.com>
2018-04-26 00:06:27 +0300

    Clean Logo code

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,32 +5,29 @@ import styles from './Logo.scss';
 import logoBackground from '../../assets/background-night-xxs.jpg';
 import { devideIntoChars } from '../../utilities';
 
-const logo = () => {
-  const firstname = 'Oleksii';
-  const lastname = 'Bespalko'
-  return (
-    <div className={styles.Logo}>
-      <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
-      <div 
+const logo = () => (
+  <div className={styles.Logo}>
+    <Animated animationIn="bounceInLeft" isVisible={true}>
+      <div
         className={styles.Logo__image}
-        style={{ backgroundImage: `
+        style={{
+          backgroundImage: `
           linear-gradient(90deg, rgba(67, 206, 162, .5), rgba(24, 90, 157, .5)), 
-          url(${logoBackground})` }}  
+          url(${logoBackground})`
+        }}
       >
         <div style={{ transform: 'rotate(-45deg)' }}>O</div>
       </div>
-      </Animated>
-      <div className={styles.Logo__name}>
-        <div className={styles.Logo__name__firstname}>
-          {devideIntoChars(firstname, 'rollIn', 5)}
-        </div>
-        <div className={styles.Logo__name__lastname}>
-          {devideIntoChars(lastname, 'fadeInRightBig', 15)}
-        </div>
+    </Animated>
+    <div className={styles.Logo__name}>
+      <div className={styles.Logo__name__firstname}>
+        {devideIntoChars('Oleksii', 'rollIn', 5)}
+      </div>
+      <div className={styles.Logo__name__lastname}>
+        {devideIntoChars('Bespalko', 'fadeInRightBig', 15)}
       </div>
     </div>
-    
-  );
-};
+  </div>
+);
 
 export default logo;
--- src/utilities.js
+++ src/utilities.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Animated } from 'react-animated-css';
+
 // Returns random number up to 'limit'
 export const random = limit => Math.floor(Math.random() * limit);
 
@@ -10,12 +11,16 @@ export const checkDayTime = () => {
   return time.getHours() > 5 && time.getHours() < 19 ? 'day' : 'night';
 };
 
-export const devideIntoChars = (word, animatedClassName, delayIndex) => {
-  return word.split('').map((letter, index) => (
+// Devides a word into characters with 'span' elements
+export const devideIntoChars = (word, animatedClassName, delayIndex) => (
+  // word - String to devide into chars
+  // animatedClassName - String of className from 'animate.css'
+  // delayIndex - Number divider for consistent animation delay
+  word.split('').map((letter, index) => (
     <Animated key={index} animationIn={animatedClassName} style={{ animationDelay: `${index / delayIndex}s` }} isVisible={true}>
-      <div>
+      <span>
         {letter}
-      </div>
+      </span>
     </Animated>
-  ));
-};
+  ))
+);
