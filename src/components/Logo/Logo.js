0eb8f8a3f83f10e51e31a28b1f0a72ad02e1f3cb
Oleksii Bespalko <ol.bespalko@gmail.com>
2018-04-25 23:40:29 +0300

    Animate logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
+import { Animated } from 'react-animated-css';
 import styles from './Logo.scss';
 import logoBackground from '../../assets/background-night-xxs.jpg';
-
 import { devideIntoChars } from '../../utilities';
 
 const logo = () => {
@@ -10,6 +10,7 @@ const logo = () => {
   const lastname = 'Bespalko'
   return (
     <div className={styles.Logo}>
+      <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
       <div 
         className={styles.Logo__image}
         style={{ backgroundImage: `
@@ -18,12 +19,13 @@ const logo = () => {
       >
         <div style={{ transform: 'rotate(-45deg)' }}>O</div>
       </div>
+      </Animated>
       <div className={styles.Logo__name}>
         <div className={styles.Logo__name__firstname}>
-          {devideIntoChars(firstname, styles.letter, true)}
+          {devideIntoChars(firstname, 'rollIn', 5)}
         </div>
         <div className={styles.Logo__name__lastname}>
-          {lastname}
+          {devideIntoChars(lastname, 'fadeInRightBig', 15)}
         </div>
       </div>
     </div>
--- src/utilities.js
+++ src/utilities.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import { Animated } from 'react-animated-css';
 // Returns random number up to 'limit'
 export const random = limit => Math.floor(Math.random() * limit);
 
@@ -8,17 +10,12 @@ export const checkDayTime = () => {
   return time.getHours() > 5 && time.getHours() < 19 ? 'day' : 'night';
 };
 
-export const sequentialAnimationDelay = (dividend) => ({ animationDelay: `${dividend / 5}s`});
-
-export const devideIntoChars = (word, classname, animate) => {
-  return word.split('').map((letter, index) => {
-    return (
-      <span key={index}
-        className={classname}
-        style={animate ? sequentialAnimationDelay(index) : null}
-      >
+export const devideIntoChars = (word, animatedClassName, delayIndex) => {
+  return word.split('').map((letter, index) => (
+    <Animated key={index} animationIn={animatedClassName} style={{ animationDelay: `${index / delayIndex}s` }} isVisible={true}>
+      <div>
         {letter}
-      </span>
-    );
-  });
+      </div>
+    </Animated>
+  ));
 };
