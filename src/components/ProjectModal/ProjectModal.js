ca511b989fbb72b69812d47012147f6cea2b21b3
Oleksii Bespalko <ol.bespalko@gmail.com>
2018-05-02 11:37:21 +0300

    Fix ProjectModal description displaying

--- src/components/ProjectModal/ProjectModal.js
+++ src/components/ProjectModal/ProjectModal.js
@@ -6,11 +6,13 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 
 const projectModal = (props) => (
   <div className={styles.ProjectModal}>
-    <div 
-      className={styles.ProjectModal__image} 
-      style={{ backgroundImage: `url(${props.image})` }}
-    >
-    </div>
+    <a className={styles.ProjectModal__image} style={{ backgroundImage: `url(${props.image})` }} href={props.app} target="_blank" rel="noopener noreferrer">
+      <div 
+        // className={styles.ProjectModal__image} 
+       
+      >
+      </div>
+    </a>
     <div className={styles.ProjectModal__data}>
       <header className={styles.data__header}>
         <h3 className={styles.data__header__title}>{props.title}</h3>
@@ -18,7 +20,16 @@ const projectModal = (props) => (
           &times;
         </div>
       </header>
-      <div className={styles.data__description}>{props.description}</div>
+      <div className={styles.data__description}>
+        {
+          props.description.length > 300 ?
+            <React.Fragment>
+              {props.description.slice(0, 297)}
+              <Button buttonType="anchor" textLink href={props.app} newtab>...</Button>
+            </React.Fragment> :
+            props.description
+        }
+      </div>
       <ul className={styles.data__toolBox}>
         {props.tools.map((tool, index) => <li className={styles.data__toolBox__tool} key={index}>{tool}</li>) }
       </ul>
